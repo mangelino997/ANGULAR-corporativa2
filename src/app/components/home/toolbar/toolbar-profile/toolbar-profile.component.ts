7889779e99ef03d68ef27419b9a8709eb595e886
Blas Bravo <blas@draimo.com>
2019-01-24 12:36:31 -0300

    [Login] Se desarrolló html, falta autenticación contra el servicio web.

--- src/app/components/home/toolbar/toolbar-profile/toolbar-profile.component.ts
+++ src/app/components/home/toolbar/toolbar-profile/toolbar-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar-profile',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar-profile.component.scss']
 })
 export class ToolbarProfileComponent implements OnInit {
-
+  //Evento que envia los datos del formulario al componente padre
+  @Output() dataEvent = new EventEmitter<any>();
+  //Constructor
   constructor() { }
-
+  //Al inicializarse el componente
   ngOnInit() {
   }
-
+  //Envia los datos al componente padre
+  public sendData(state): void {
+    this.dataEvent.emit(state);
+  }
+  //Logout
+  public logout(): void {
+    this.sendData(false);
+  }
 }
